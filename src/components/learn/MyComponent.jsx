328e87315cac28223a1374886abe2842df8c6a93
huybear5198 <vuquochuy5198@gmail.com>
2025-07-20 16:09:20 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,22 @@
 import './style.css';
 
 const MyComponent = () => {
+    //const name = "HuyBear";
+    //const age = 27;
+    const infor = {
+        name: 'Huybear',
+        age: 27,
+    }
+
+    const printObject = {
+        id: 1,
+        objectname: 'Object 1'
+    }
     return (
         <>
-            <div>MY FIRST COMPONENT</div>
+            <div>Hello {infor.name}, I'm {infor.age}</div>
+            <div>{JSON.stringify(printObject)}</div>
+            <div>{console.log('Javascript on JSX')}</div>
             <div className='child' style={
                 { borderRadius: "10px" }
             }
