6498c0dda50d74615b8b7c1eef1693552e496c4a
huybear5198 <vuquochuy5198@gmail.com>
2025-07-22 10:05:10 +0700

    video 33

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+    return (
+        <div className='todo-data'>
+            <div>Learning ReactJS</div>
+            <div>Watch Youtube</div>
+        </div>
+    );
+}
+
+export default TodoData;
